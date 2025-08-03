bbcb0d65a9fd95a043f81f63d5b19db198beab2c
provash10 <provash20cb@gmail.com>
2025-08-04 02:49:28 +0600

    Assignment 03 Practice

---
+++ area.js
@@ -0,0 +1,7 @@
+/** Problem -01 ( Divide the Asset ) */
+var area = 100;
+//write your code here
+var twoBrothers = 2;
+var result = area / twoBrothers;
+
+console.log(result); 
---
+++ cycle-laptop.js
@@ -0,0 +1,21 @@
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+
+if(money >=25000){
+    console.log('Laptop');
+}
+
+else if(money >=10000){
+    console.log('Cycle');
+}
+
+// else if(money <=10000){
+//     console.log('Chocolate');
+// }
+
+else{
+    console.log('Chocolate');
+}
+
+
