e6c5fbd303b1a50ef1c6ce57417a3fc35d0bfbd5
provash10 <provash20cb@gmail.com>
2025-08-04 20:12:51 +0600

    A03 complete

--- medicine.js
+++ medicine.js
@@ -5,9 +5,11 @@ var lastDay = 11 ;
 //write your code here
 for( var i = 1; i <=lastDay; i++){
     if((i-3) % 3 === 0) {
-        console.log(i + " - medicine");
+        // console.log(i + " - medicine");
+        console.log(i + " - " + "medicine" );
     }
     else{
-         console.log(i + " - rest");
+        //  console.log(i + " - rest");
+        console.log(i + " - " + "rest" );
     }
 }
